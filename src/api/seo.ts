a0fe2dc3e4b19bc54553a24a01cad715ad7d9aea
aqsa <gulfinvestaemarketing@gmail.com>
2024-12-01 09:24:30 +0400

    make dynamic pages

--- src/api/seo.ts
+++ src/api/seo.ts
@@ -1,5 +1,5 @@
 export async function getMetaDataByPage(page: string) {
-    const metaData = await fetch(`${process.env.API_HOST}meta/${page}`, {
+    const metaData = await fetch(`https://treystabackend.deepakpp.live/api/meta/${page}`, {
       cache: "force-cache",
       next: { tags: ["meta", `meta:${page}`, "meta:*"] },
     })
