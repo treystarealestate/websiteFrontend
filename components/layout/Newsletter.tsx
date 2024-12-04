fe8c07a18917126b9502d9651c9110f14cc60362
Aqsa <treystarealestate@gmail.com>
2024-12-04 21:11:24 +0400

    homee update

--- components/layout/Newsletter.tsx
+++ components/layout/Newsletter.tsx
@@ -59,24 +59,14 @@ const Newsletter = () => {
                             <div className="col-12 col-lg-6 col-md-7">
                                 <div className='pb-5 pb-lg-0 pb-md-0'>
 
-                                {isMobileDev ? (
+                               
                                             <div>
                                     <div className="descCont text-center pb-3 ">
                                         <h4 className="text-head text-black fw-bold">Stay Updated</h4>
                                         <p className="text-desc">Unlock new realms of real estate. Get instant access to market news, trends, and updates in your inbox.</p>
                                     </div>
                                     </div>
-                                        ) : (
-                                            <motion.div variants={introTopVariants} // Use the header animation variants
-                                            initial="hide"
-                                            whileInView="show"
-                                            transition={{ duration: 1 }}>
-                                    <div className="descCont text-center pb-3 ">
-                                        <h4 className="text-head text-black fw-bold">Stay Updated</h4>
-                                        <p className="text-desc">Unlock new realms of real estate. Get instant access to market news, trends, and updates in your inbox.</p>
-                                    </div>
-                                    </motion.div>
-                                    )}
+                                      
                                     <div>
                                         <form action="" method="post" className='newsletterForm' onSubmit={handleSubmit(onSubmit)}>
                                             <div className="input-group">
