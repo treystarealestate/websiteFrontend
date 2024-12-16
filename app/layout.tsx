31c1faea5dd07dcb1fde9251f849101c874bfdb4
treya <treystarealestate@gmail.com>
2024-12-16 14:15:41 +0400

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,11 +35,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               : metadata.keywords || "" // Use as-is if it's a string, fallback if null or undefined
           }
         />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"></link>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      
       </Head>
       <body>
         <CustomNavbar />
