08dba731c0da5cba7fb8ef307025e8d9f947e6c5
treystarealestate <treystarealestate@gmail.com>
2025-04-10 22:31:54 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,18 +7,23 @@ import CustomNavbar from "@/components/layout/Navbar";
 import Footer from '@/components/layout/Footer';
 import Script from "next/script";
 
-// Example metadata
+
 export const metadata: Metadata = {
   title: "Treysta",
   description: "Where Trust Meets Fun in Real Estate",
   keywords: ["Rent", "Buy", "Rent"], // Array of strings
 };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <Head>
-        {/* Ensure title is a string */}
-        <title>{String(metadata.title)}</title>
+      <head>
+       {/* Ensure title is a string */}
+       <title>{String(metadata.title)}</title>
 
         {/* Ensure description is a string */}
         <meta
@@ -35,22 +40,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               : metadata.keywords || "" // Use as-is if it's a string, fallback if null or undefined
           }
         />
-      <Script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-5N3T8V89');
-              `,
-            }}
-          />
-  
-      </Head>
+        <Script
+          id="google-tag-manager-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5N3T8V89');
+          `,
+          }}
+        />
+        
+        <meta name="robots" content="index,follow" />
+      </head>
       <body>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5N3T8V89"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>        
+    
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5N3T8V89"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <CustomNavbar />
         {children}
          <Footer/>
