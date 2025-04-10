1a1af5ba20794da839d98b9d58ba28de6e45f390
treystarealestate <treystarealestate@gmail.com>
2025-04-10 22:19:14 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,28 +36,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           }
         />
       
-        {/* Google Tag Manager */}
-        <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-5N3T8V89');
-              `,
-            }}
-          />
-        {/* End Google Tag Manager */}
   
       </Head>
       <body>
       
-        {/* GTM noscript */}
-        <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5N3T8V89"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+    
         
         <CustomNavbar />
         {children}
