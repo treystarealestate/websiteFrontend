17d47ec60cbd0514f97fd96145b2b6b412ac369e
treystarealestate <treystarealestate@gmail.com>
2025-04-10 22:21:19 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,13 +35,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               : metadata.keywords || "" // Use as-is if it's a string, fallback if null or undefined
           }
         />
-      
+      <Script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5N3T8V89');
+              `,
+            }}
+          />
   
       </Head>
       <body>
-      
-    
-        
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5N3T8V89"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>        
         <CustomNavbar />
         {children}
          <Footer/>
