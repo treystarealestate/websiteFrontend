0d9fde76700e5088d6cc180a83f858d643585aa2
treystarealestate <treystarealestate@gmail.com>
2025-06-26 20:12:47 +0400

    Update home.ts

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,5 +1,7 @@
 export async function getHomeData() {
-    const response = await fetch(`${process.env.API_HOST}homeData`);
+    const response = await fetch(`${process.env.API_HOST}homeData`, {
+    cache: 'no-store', // disables static caching
+  });
   
     if (!response.ok) return undefined;
   
