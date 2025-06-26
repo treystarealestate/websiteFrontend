b5e9a4e915800bcdef7cace53a80a7422b52fc45
treystarealestate <treystarealestate@gmail.com>
2025-06-26 19:52:18 +0400

    Update ProjectDetails.tsx

--- components/off-plan/ProjectDetails.tsx
+++ components/off-plan/ProjectDetails.tsx
@@ -32,38 +32,32 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = ({ projectData }) => {
     <div>
       <div className="bg-darkGold p-3 rounded-4 mb-3">
         <Row className="justify-content-center g-3">
-          <Col xs="12" lg="4">
+          <Col xs="12" lg="3">
             <p className="mb-0 fs-14">Starting from: <span className="fw-bold">AED {startingPrice}</span></p>
           </Col>
-          <Col xs="12" lg="4">
+          <Col xs="12" lg="3">
             <p className="mb-0 fs-14">Starting Size: <span className="fw-bold">{startingSize} Sq.Ft</span></p>
 
           </Col>
-          <Col xs="12" lg="4">
+          <Col xs="12" lg="3">
             <p className="mb-0 fs-14">Bedrooms: <span className="fw-bold">{bedrooms}</span></p>
 
           </Col>
-          <Col xs="12" lg="4">
+          <Col xs="12" lg="3">
             <p className="mb-0 fs-14">Property Type: <span className="fw-bold">{propertyType}</span></p>
           </Col>
-          <Col xs="12" lg="4">
+          <Col xs="12" lg="3">
             <p className="mb-0 fs-14">Developer: <span className="fw-bold">{developer}</span></p>
 
           </Col>
-          <Col xs="12" lg="4">
+          <Col xs="12" lg="3">
             <p className="mb-0 fs-14">Handover: <span className="fw-bold">{handoverDate}</span></p>
           </Col>
-          
-          
-        </Row>
-        <Row className="justify-content-center g-3">
-        {permit_number && (
-            <Col xs="12" lg="4">
+           <Col xs="12" lg="3">
               <p className="mb-0 fs-14">Permit Number: <span className="fw-bold">{permit_number}</span></p>
             </Col>
-            
-          )}
         </Row>
+        
       </div>
       <div className="descCont text-para">
       
